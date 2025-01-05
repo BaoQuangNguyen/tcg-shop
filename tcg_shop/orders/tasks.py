626b547394f74630c17d12efb74e7a52ac6b3798
BaoQuangNguyen <bnguyen0239@gmail.com>
2025-01-05 15:12:41 -0600

    feat(orders): Initialized async features with celery

--- tcg_shop/orders/tasks.py
+++ tcg_shop/orders/tasks.py
@@ -6,6 +6,10 @@ from .models import Order
 
 @shared_task
 def order_created(order_id):
+    """
+    Task to send an e-mail notification when an order is
+    successfully created.
+    """
     order = Order.objects.get(id=order_id)
     subject = f'Order nr. {order.id}'
     message = (
--- tcg_shop/orders/views.py
+++ tcg_shop/orders/views.py
@@ -20,7 +20,7 @@ def order_create(request):
                     quantity=item['quantity'],
                 )
             cart.clear()
-            order_created.delay(order.id)
+            order_created.delay_on_commit(order.id)
             return render(
                 request,
                 'orders/order/created.html',
