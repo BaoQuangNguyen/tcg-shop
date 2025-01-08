1f490b50ca339935021197ecf1a53e161b606078
BaoQuangNguyen <bnguyen0239@gmail.com>
2025-01-08 11:03:03 -0600

    fix(css): modified shop/product/detail.html

--- tcg_shop/cart/forms.py
+++ tcg_shop/cart/forms.py
@@ -6,11 +6,19 @@ class CartAddProductForm(forms.Form):
     quantity = forms.TypedChoiceField(
         choices=PRODUCT_QUANTITY_CHOICES,
         coerce=int,
-        label='Quantity',
-        
+        label='',
+        widget=forms.NumberInput(
+            attrs={
+                'class': 'w-20 py-2 px-3 border-2 border-pink-500 rounded-lg focus:ring-2 focus:ring-pink-500 focus:border-pink-500 text-lg font-medium text-pink-500',
+                'min': '1',
+                'max': '20',
+                'value': '1',
+                'step': '1'
+            }
+        )
     )
     override = forms.BooleanField(
         required=False,
         initial=False,
-        widget=forms.HiddenInput,
+        widget=forms.HiddenInput
     )
